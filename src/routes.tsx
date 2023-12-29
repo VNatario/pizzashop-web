5be9853a3f7876f1f0460ca0adfc2cdf4ef8a128
Vitor Natario <natariovitor@gmail.com>
2023-12-29 17:16:05 -0300

    feat: restaurant registration

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 import { AppLayout } from './pages/layouts/app'
 import { AuthLayout } from './pages/layouts/auth'
 
@@ -9,21 +10,14 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
-    ],
+    children: [{ path: '/', element: <Dashboard /> }],
   },
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
     children: [
-      {
-        path: '/sign-in',
-        element: <SignIn />,
-      },
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ],
   },
 ])
